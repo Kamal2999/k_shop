a79ca7eed72b38a4097afd4f52a20fa8d1f769cb
kamal2999 <kamalmattu744@gmail.com>
2026-06-05 10:56:27 +0530

    some changes

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -52,43 +52,76 @@ export class Register {
 
   // REGISTER FUNCTION
   register(form: NgForm) {
+
     this.message = '';
     this.messageType = '';
+
     if (form.invalid) {
+
       Object.keys(form.controls).forEach(field => {
         form.controls[field].markAsTouched();
       });
+
       return;
     }
+
     if (this.password !== this.confirmPassword) {
+
       this.messageType = 'error';
       this.message = 'Passwords do not match';
+
       return;
     }
+
     const data = {
       name: this.name,
       email: this.email,
       phone: this.phone,
       password: this.password
     };
+
     console.log(data);
+
     this.api.register(data).subscribe({
+
       next: (res: any) => {
+
         console.log(res);
-        this.messageType = 'success';
-        this.message = res.message || 'Register Successfull Please Login And Enjoy Shopping';
-        setTimeout(() => {
-          this.router.navigate(['/login']);
-        }, 1500);
+
+        if (res.status) {
+
+          this.messageType = 'success';
+
+          this.message =
+            res.message ||
+            'Register Successful Please Login And Enjoy Shopping';
+
+          setTimeout(() => {
+            this.router.navigate(['/login']);
+          }, 1500);
+
+        } else {
+
+          this.messageType = 'error';
+
+          this.message =
+            res.message || 'Registration Failed';
+        }
       },
+
       error: (err: any) => {
+
         console.log(err);
+
         this.messageType = 'error';
+
         this.message =
           err?.error?.message ||
           'Registration Failed';
       }
+
     });
+
   }
 
 }
